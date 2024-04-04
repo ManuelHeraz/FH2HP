6498c88794a646d4657f75cfcea5d3d582eea8b4
ManuelHeraz <hernandezdiazjm00@gmail.com>
2024-04-03 18:10:34 -0600

    v24.04.03.0641

---
+++ assets/js/music-list.js
@@ -0,0 +1,251 @@
+let allMusic = [
+    
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-1" //name here
+    },
+
+    {
+        name: "Lazyboy - Pica Disco",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-2-Disco" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-3" //name here
+    },
+
+    {
+        name: "Jetta - Operators",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-4-Operators" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-5" //name here
+    },
+
+    {
+        name: "Tensnake - Love Sublime",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-6-LoveSublime" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-7" //name here
+    },
+
+    {
+        name: "Pyramid - The Phoenix",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-8-Phoenix" //name here
+    },
+
+    {
+        name: "Digitalism - Wolves",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-9-Wolves" //name here
+    },
+
+    {
+        name: "Roosevelt - Sea",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-10-Sea" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-11" //name here
+    },
+
+    {
+        name: "Bo Saris - Can+t Beat The Old Skool",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-12-Skool" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-13" //name here
+    },
+
+    {
+        name: "Saint Raymond - Brighter",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-14-Brighter" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-15" //name here
+    },
+
+    //Imagenes
+
+    {
+        name: "Cut Copy - Free Your Mind",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-16-Free" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-17" //name here
+    },
+
+    {
+        name: "Little Dragon - Klapp Klapp",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-18-Klapp" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-19" //name here
+    },
+
+    {
+        name: "1- Jungle - The Heat / 2- Panama - Always",//music name
+        artist: "3- Chromeo - Jealous", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-20-Mix" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-21" //name here
+    },
+
+    {
+        name: "Phonat - Never",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-22-Never" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-23" //name here
+    },
+
+    {
+        name: "Jungle - Busy Earnin´",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-24-Busy" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-25" //name here
+    },
+
+    {
+        name: "Bombay Bicycle Club - Luna",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-26-Luna" //name here
+    },
+
+    {
+        name: "Chromeo - Come Alive",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-27-Come" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-28" //name here
+    },
+
+    {
+        name: "Architecture in Helsinki - Might Survive",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-29-Survive" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-30" //name here
+    },
+
+    {
+        name: "PNAU - With You Forever",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-31-Forever" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-32" //name here
+    },
+
+    {
+        name: "Holy Ghost! - Bridge and Tunnel",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-33-Bridge" //name here
+    },
+
+    {
+        name: "Horiozon Pulse Radio Oficial del fetival Horizon 2",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-34" //name here
+    },
+
+    {
+        name: "CHVRCHES - The Mother We Share",//music name
+        artist: "Horizon Pulse", // artist name
+        img:"Hate-TDG", // image name
+        src: "PulseH2-35-Mother" //name here
+    },
+
+
+];
